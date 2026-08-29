ec5fadb2b5f2fae7e4775740f1936189cd505469
Matt Oliveira <matt.d.oliveira@gmail.com>
2026-08-29 15:35:06 -0400

    records added to SteamClient

--- server/src/main/java/learn/unlockt/data/SteamClient.java
+++ server/src/main/java/learn/unlockt/data/SteamClient.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Component
 public class SteamClient {
@@ -41,9 +43,22 @@ public class SteamClient {
         return Optional.of(result.steamid());
     }
 
+//    public Optional<List<GameRecord>> getOwnedGames(UUID id) {
+//
+//        return new List<GameRecord>;
+//    }
+
+
     private record VanityUrlResponse(VanityUrlResult response) {
     }
 
     private record VanityUrlResult(String steamid, int success) {
     }
+
+    private record OwnedGamesResponse(OwnedGamesResult response) {}
+
+    private record OwnedGamesResult(int game_count, List<GameRecord> games) {}
+
+    private record GameRecord(int app_idt, String name, boolean has_community_visible_stats) {}
+
 }
